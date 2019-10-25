2e310381fbf3cacf382840dc7a41696b8e2cb970
Ilya Petrov <dnamix1@gmail.com>
2019-10-25 18:23:27 +0300

    css modules has been added to workflow

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,23 @@
 import React from "react";
+import s from "./Navbar.module.css";
 
 export default function Navbar() {
   return (
     <div>
-      <nav className="nav">
-        <div>
+      <nav className={s.nav}>
+        <div className={`${s.item} ${s.active}`}>
           <a href="/profile">Profile</a>
         </div>
-        <div>
+        <div className={s.item}>
           <a href="/dialogs">Messages</a>
         </div>
-        <div>
+        <div className={s.item}>
           <a href="/news">News</a>
         </div>
-        <div>
+        <div className={s.item}>
           <a href="/music">Music</a>
         </div>
-        <div>
+        <div className={s.item}>
           <a href="/settings">Settings</a>
         </div>
       </nav>
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import hero from "../assets/hero.jpg";
+import s from "./Profile.module.css";
 
 export default function Content() {
   return (
     <div>
-      <div className="content">
+      <div className={s.content}>
         <img src={hero} alt="hero banner" />
         <div>Ava + Description</div>
-        <div>
+        <div className={s.posts}>
           My Posts
-          <div>New Posts</div>
-          <div>post 1</div>
-          <div>post 2</div>
+          <div className={s.item}>post 1</div>
+          <div className={s.item}>post 2</div>
         </div>
       </div>
     </div>
