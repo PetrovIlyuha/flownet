ff8fc6b1d40bfe4d23d81c44e5469c36da0b2e4e
Ilya Petrov <dnamix1@gmail.com>
2019-11-15 20:45:26 +0300

    started fetching users from API

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,45 +1,16 @@
 import React from "react";
 import s from "./users.module.css";
 import { Button } from "reactstrap";
+import * as axios from "axios";
+import userPhoto from '../../assets/friend0.jpg';
 
 const Users = (props) => {
-  if (props.users.length === 0){
-    props.setUsers( [
-      {
-        id: 1,
-        photoUrl: require("../../assets/friend0.jpg"),
-        followed: false,
-        fullName: "Bob Marley",
-        status: "Singer",
-        location: {
-          city: "Minneapolis",
-          country: "USA"
-        }
-      },
-      {
-        id: 2,
-        photoUrl: require("../../assets/friend1.jpg"),
-        followed: true,
-        fullName: "Ricky Martin",
-        status: "Singer",
-        location: {
-          city: "Calcutta",
-          country: "India"
-        }
-      },
-      {
-        id: 3,
-        followed: false,
-        photoUrl: require("../../assets/friend2.jpg"),
-        fullName: "Vladimir Putin",
-        status: "President",
-        location: {
-          city: "Moscow",
-          country: "Russia"
-        }
-      }
-    ]);
+  if (props.users.length === 0) {
+    axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+      props.setUsers(response.data.items);
+    });
   }
+
   return (
     <div className={s.users}>
       {
@@ -48,22 +19,26 @@ const Users = (props) => {
             <div key={user.id} className={s.userContainer}>
               <div className={s.avatar}>
                 <img
-                  src={user.photoUrl}
-                  alt="photo"
+                  src={user.photos.small !== null ? user.photos.small : userPhoto}
+                  alt="user photography"
                   style={{ width: "70px", borderRadius: "50%", border: "3px solid palegreen" }}
                 />
               </div>
               <div>
                 {
                   user.followed
-                    ? <Button onClick={() => { props.unfollow(user.id)}}>Unfollow</Button>
-                    : <Button outline color="secondary" onClick={() => { props.follow(user.id)}}>Follow</Button>
+                    ? <Button onClick={() => {
+                      props.unfollow(user.id);
+                    }}>Unfollow</Button>
+                    : <Button outline color="secondary" onClick={() => {
+                      props.follow(user.id);
+                    }}>Follow</Button>
                 }
               </div>
-              <div className={s.name}>{user.fullName}</div>
+              <div className={s.name}>{user.name}</div>
               <div className={s.status}>{user.status}</div>
-              <div className={s.country}>{user.location.country}</div>
-              <div className={s.city}>{user.location.city}</div>
+              <div className={s.country}>{"user.location.country"}</div>
+              <div className={s.city}>{"user.location.city"}</div>
             </div>
           );
         })
