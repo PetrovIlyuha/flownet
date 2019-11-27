31222b37b9fbbfca48345dcdd1cfe15ba194706c
Ilya Petrov <dnamix1@gmail.com>
2019-11-27 15:07:13 +0300

    replaced simple incoming props in Users mapStateToProps for separate selectors contained in distinctive module

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -9,6 +9,14 @@ import {
 import Users from "./Users";
 import Preloader from "../Common/Preloader/preloader";
 import {withAuthRedirect} from '../../HOC/withAuthRedirect';
+import {
+  getCurrentPage,
+  getFollowingInProgress,
+  getIsFetching,
+  getPageSize,
+  getTotalUsersCount,
+  getUsersFromState
+} from "./usersSelectors";
 
 
 class UsersContainer extends React.Component {
@@ -40,12 +48,12 @@ class UsersContainer extends React.Component {
 
 const mapStateToProps = (state) => {
   return {
-    users: state.usersPage.users,
-    pageSize: state.usersPage.pageSize,
-    totalUsersCount: state.usersPage.totalUsersCount,
-    currentPage: state.usersPage.currentPage,
-    isFetching: state.usersPage.isFetching,
-    followingInProgress: state.usersPage.followingInProgress
+    users: getUsersFromState(state),
+    pageSize: getPageSize(state),
+    totalUsersCount: getTotalUsersCount(state),
+    currentPage: getCurrentPage(state),
+    isFetching: getIsFetching(state),
+    followingInProgress: getFollowingInProgress(state)
   };
 };
 
